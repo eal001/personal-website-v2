8563fc7f4410a083e08c8784767290a482dabeea
Elliot Alexander Lee <eal001@ucsd.edu>
2021-12-20 20:01:32 -0800

    adding in projects

--- components/ProjectsCard/index.tsx
+++ components/ProjectsCard/index.tsx
@@ -1,17 +1,39 @@
+import projects from  "../../projects.config.js"
+
 const Projects = () => {
-    return(
+    return (
         <div id='projects'>
             <div id="card">
                 <h1>My Work</h1>
+                <div id='center-container'>
+                    <div id='container'>
+                        {
+                            projects.map(project => {
+                                return (
+                                    <Project image={project.image} name={project.name} description={project.description} link={project.link}/>   
+                                )
+                            })
+                        }
+                    </div>
+                </div>
             </div>
         </div>
+        
     )
 }
 
-const Project = ({image, name, description}) => {
-    <div id={'project '+name}>
-        <img src={image} alt={name}></img>
-    </div>
+const Project = ({image, name, description, link}) => {
+
+    return (
+        <div id='container'>
+        <div id={'project'} >
+            <img src={image.src} alt={name}></img>
+            <a id='wrapper' href={link}>
+                <p>{description}</p>
+            </a>
+        </div>
+        </div>
+    )
 }
 
 export default Projects;
---
+++ projects.config.js
@@ -0,0 +1,40 @@
+import rateMyLease from "./public/RateMyLease_JChoe.png";
+import biasML from "./public/BiasML_by_SSerafin.png";
+import workoutApp from "./public/WorkoutApp_icon.png";
+import novellyMarket from "./public/novellylogo1.png";
+import ucsdLogo from "./public/UCSD_logo.jpeg";
+
+const projects = [
+    {
+        "name": "BiasML",
+        "image": biasML,
+        "description": "BiasML is a full stack web app. It uses React as a frontend and Google Firebase backend as a service. My team and I implemented webscraping and natural language machine learning to analyze the political bias of web articles.",
+        "link": "https://devpost.com/software/bias-ml"
+    },
+    {
+        "name": "RateMyLease",
+        "image": rateMyLease,
+        "description": "RateMyLease is a fullstack web app. It assists UC Davis students with finding leases especially after the Covid-19 Lockdown. My team and I use a React frontend, an Express backend, and a MongoDB database.",
+        "link": "https://github.com/jiuchoe4/rate-my-lease"
+    },
+    {
+        "name": "RateMyLease",
+        "image": workoutApp,
+        "description": "A personal passion project: to create a mobile app to log my workouts. I used Swift and iOS Cocoapods Libraries to create an iOS app that I use daily top log my workout routines. This app is a work-in-progress.",
+        "link": "https://github.com/eal001/Workout-App"
+    }, 
+    {
+        "name": "Novelly Marketing Website",
+        "image": novellyMarket,
+        "description": "I built Novelly's marketing website using Webflow and the professional advice of designer Nicole Davenport and product manager Shantelle Serafin. It describes Novelly's mission to empower young readers and writers.",
+        "link": "https://www.novelly.org/"
+    }, 
+    {
+        "name": "Data Science in Practice",
+        "image": ucsdLogo,
+        "description": "My team and I took large scale, real world data and used pandas, sklearn, and nltk to answer the question \"Is the volume a stock trades at affected by Twitter?\" This class project was supervised by the esteemed Jason Fliescher.",
+        "link": "https://github.com/COGS108/Group002-Fa21"
+    }
+]
+
+export default projects;
